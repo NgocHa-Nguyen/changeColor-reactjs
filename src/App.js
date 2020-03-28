7584b10a9d1cf4fced966821a762d4e0761fcc28
NgocHa-Nguyen <ngochadut28@gmail.com>
2020-03-28 07:25:35 +0700

    Chia components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ColorPicker from './components/ColorPicker';
+import SizeSetting from './components/SizeSetting';
+import Result from './components/Result';
+import Reset from './components/Reset';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      color : 'red',
+      fonSize : 16
+    }
+
+  }
+
+  onSetColor(parmas){
+    console.log(parmas)
+  }
+  render() {
+    
+    return (
+      <div className="container mt-50">
+        <div className="row">
+          <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor}></ColorPicker>
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <SizeSetting></SizeSetting>
+            <Reset></Reset>
+          </div>
+          <Result></Result>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+class ColorPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colors: ['red', 'green', 'blue', '#ccc']
+        };
+    }
+    showColor(color) {
+        return {
+            backgroundColor: color
+        }
+    }
+    setActiveColor(){
+        this.props.onReceiveColor(color);
+    }
+    render() {
+        var elmColoers = this.state.colors.map((color, index) => {
+            return <span key={index} className={this.props.color === color ? 'active': ''} style={this.showColor(color)} onClick={()=> this.setActiveColor(color)}></span>
+        })
+        return (
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <div className="panel panel-primary">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Color Picker</h3>
+                    </div>
+                    <div className="panel-body">
+                        {elmColoers}
+                        <span onClick={() => this.setActiveColor(color)}></span>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ColorPicker;
